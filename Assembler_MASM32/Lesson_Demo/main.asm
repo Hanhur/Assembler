37fe4c3dba92d3530b1442ad7cc9548eba8ef490
Vladimir Hanhur <aqgz@email.cz>
2025-04-16 09:38:36 +0200

    update

--- Assembler_MASM32/Lesson_Demo/main.asm
+++ Assembler_MASM32/Lesson_Demo/main.asm
@@ -6,9 +6,17 @@ option casemap:none
 include \masm32\include\kernel32.inc
 includelib \masm32\lib\kernel32.lib
 ;====================================
+;Constant
+.const
+	FPS equ 1000
+	
+; Sekcie data
 .data
-	numberA dd 10
-	numberB dd 11
+	numberA db 10 ; +0
+	numberB dd 11 ; +1
+	numberC dd 12 ; +3
+	;numbreD dq 13 ; +12
+	;littleEndians dd 12345678h
 	
 .data?
 	hInstance dd ?
@@ -16,9 +24,11 @@ includelib \masm32\lib\kernel32.lib
 ; Sekcie koda
 .code
 start:
+	;mov eax, littleEndians
+	;----------------
 	mov hInstance, -1
 	;---------------
-	push 3000
+	push FPS
 	;---------------
 	; function Sleep, zaderzhka 3000 milisekund
 	call Sleep
