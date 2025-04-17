4f611bca3a7f1aad88aa51a8673e3f580d9ef471
Vladimir Hanhur <aqgz@email.cz>
2025-04-17 11:55:30 +0200

    update

--- Assembler_MASM32/Lesson_Demo/main.asm
+++ Assembler_MASM32/Lesson_Demo/main.asm
@@ -12,21 +12,62 @@ includelib \masm32\lib\kernel32.lib
 	
 ; Sekcie data
 .data
-	numberA db 10 ; +0
-	numberB dd 11 ; +1
-	numberC dd 12 ; +3
+	buffer dq 0
+		   dq 0
+	;numberA db 10 ; +0
+	;numberB dd 11 ; +1
+	;numberC dd 12 ; +3
 	;numbreD dq 13 ; +12
 	;littleEndians dd 12345678h
 	
 .data?
-	hInstance dd ?
+	;hInstance dd ?
 	
 ; Sekcie koda
 .code
 start:
+	mov eax, 614D5341h
+	mov ecx, 72657473h
+	mov edx, 00002179h
+	;-----------------
+	; Preficks ptr -> Pointer (Ukazatel)
+	mov dword ptr[buffer], eax
+	mov dword ptr[buffer + 4], ecx
+	mov dword ptr[buffer + 8], edx
+	;-----------------
+	; offset -> vychist oblast pamyati, i pomesti v register eax znachenie buffera 
+	mov eax, offset buffer
+	;-----------------
+	mov eax, dword ptr[buffer] ;4 bit
+	mov dx, word ptr[buffer + 4] ;2 bit
+	mov cl, byte ptr[buffer + 6] ;1 bit
+	;-----------------
+	movzx eax, byte ptr[buffer]
+	movzx edx, word ptr[buffer]
+	;-----------------
 	;mov eax, littleEndians
 	;----------------
-	mov hInstance, -1
+	;mov hInstance, -1
+	;---------------
+	;mov al, 78h
+	;mov ah, 56h
+	;mov ax, 5678h
+	;mov eax, 12345678h
+	;---------------
+	;mov cl, 78h
+	;mov ch, 56h
+	;mov cx, 5678h
+	;mov ecx, 12345678h
+	;---------------
+	;mov dl, 78h
+	;mov dh, 56h
+	;mov dx, 5678h
+	;mov edx, 12345678h
+	;---------------
+	;mov bl, 78h
+	;mov bh, 56h
+	;mov bx, 5678h
+	;mov ebx, 12345678h
 	;---------------
 	push FPS
 	;---------------
@@ -40,4 +81,3 @@ start:
 	call ExitProcess
 	;---------------
 end start
-;Lesson 6
